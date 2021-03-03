e29c0ebde938838e720f82ad7b74a263571cda67
지농 <jinhong0719@naver.com>
2021-03-03 19:12:40 +0900

    [CREATE] <syscheck/repository/elasticsearch/syscheck.go> Create file and add comment

---
+++ syscheck/repository/elasticsearch/syscheck.go
@@ -0,0 +1,5 @@
+// Create package in v.1.0.0
+// elasticsearch package is for implementations of syscheck domain repository using elasticsearch
+// In practice, repository struct declaration and implementation is in this syscheck.go file
+
+package elasticsearch
