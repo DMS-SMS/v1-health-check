56657faa29ef0bf5e83a8633c2dcb1f59951ee4d
지농 <jinhong0719@naver.com>
2021-03-04 20:23:42 +0900

    [ADD] <../elasticsearch/syscheck.go> Add esRepository struct to embed

--- syscheck/repository/elasticsearch/syscheck.go
+++ syscheck/repository/elasticsearch/syscheck.go
@@ -8,6 +8,18 @@ import (
 	"github.com/elastic/go-elasticsearch/v7"
 )
 
+// esRepository contains the least information that elasticsearch repository should have in syscheck
+type esRepository struct {
+	// indexName represent name of index including esDiskCheckHistory document
+	indexName string
+
+	// indexShardNum represent shard number of index including esDiskCheckHistory document
+	indexShardNum int
+
+	// indexReplicaNum represent replica number of index to replace index when node become unable
+	indexReplicaNum int
+}
+
 // esDiskCheckHistoryRepository is to handle DiskCheckHistory model using elasticsearch as data store
 type esDiskCheckHistoryRepository struct {
 	cli *elasticsearch.Client
