ee38059cae48226a964f3dad8e3fcecb4814e59d
지농 <jinhong0719@naver.com>
2021-03-04 22:29:51 +0900

    [CREATE] <../elasticsearch/syscheck_disk.go> Create file for syscheck disk repo

---
+++ syscheck/repository/elasticsearch/syscheck_disk.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// syscheck_disk.go is file that define repository implement about disk using elasticsearch
+// disk repository struct embed esRepositoryRequiredComponent struct in ./syscheck.go file
+
+package elasticsearch
