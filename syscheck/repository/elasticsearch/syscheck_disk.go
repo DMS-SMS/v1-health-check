fcb665cc3aabf5e857de90806891e9762d7a3efd
지농 <jinhong0719@naver.com>
2021-03-04 22:42:24 +0900

    [ADD] <../elasticsearch/syscheck_disk.go> Add esDiskCheckHistoryRepository struct

--- syscheck/repository/elasticsearch/syscheck_disk.go
+++ syscheck/repository/elasticsearch/syscheck_disk.go
@@ -3,3 +3,16 @@
 // disk repository struct embed esRepositoryRequiredComponent struct in ./syscheck.go file
 
 package elasticsearch
+
+import (
+	"github.com/elastic/go-elasticsearch/v7"
+)
+
+// esDiskCheckHistoryRepository is to handle DiskCheckHistory model using elasticsearch as data store
+type esDiskCheckHistoryRepository struct {
+	// get common field from embedding esRepositoryRequiredComponent
+	esRepositoryRequiredComponent
+
+	// elasticsearch client connection injected from the outside package
+	cli *elasticsearch.Client
+}
