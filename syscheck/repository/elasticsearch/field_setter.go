3e760bc46cbb41c9960aee69b76b7e2bfeeb069b
지농 <jinhong0719@naver.com>
2021-03-04 23:28:21 +0900

    [CREATE] <../elasticsearch/field_setter.go> Create file to declar field setter

---
+++ syscheck/repository/elasticsearch/field_setter.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// field_setter.go is file that gather function that satisfy signature of FieldSetter in syscheck.go file.
+// Functions of FieldSetter type can be used by handing over to repository constructor
+
+package elasticsearch
