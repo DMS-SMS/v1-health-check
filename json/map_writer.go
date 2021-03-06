c745b2e00bb3abc80a682c54cc9082c2e70137fe
지농 <jinhong0719@naver.com>
2021-03-06 20:41:24 +0900

    [ADD] <json/map_writer.go> Add mapWriter struct & mapWriter initalizer

--- json/map_writer.go
+++ json/map_writer.go
@@ -3,3 +3,25 @@
 // map_writer.go is file that declare customized objects called MapWriter to convert map-type variable to json format.
 
 package json
+
+import (
+	"sync"
+)
+
+// mapWriter is struct to write []byte which is map type marshaled to another []byte for json
+// mapWriter implement io.Writer & io.WriterTo interface
+type mapWriter struct {
+	// buf is field that stores created bytes[] through Write method
+	buf []byte
+
+	// mu prevents other gorouting from entering after Write method call until WriteTo method call
+	mu sync.Mutex
+}
+
+// MapWriter return new pointer instance of mapWriter struct
+func MapWriter() *mapWriter {
+	return &mapWriter{
+		buf: []byte{},
+		mu:  sync.Mutex{},
+	}
+}
