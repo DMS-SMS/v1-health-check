c5aaeadd92fe8d3615ae1c9d86a7d632f6b9a26c
지농 <jinhong0719@naver.com>
2021-03-06 19:57:35 +0900

    [CREATE] <json/map_writer.go> Create file to declare MapWriter struct

---
+++ json/map_writer.go
@@ -0,0 +1,5 @@
+// Create package in v.1.0.0
+// json package is a collection of convenience objects, specific interface implementations related to json
+// map_writer.go is file that declare customized objects called MapWriter to convert map-type variable to json format.
+
+package json
