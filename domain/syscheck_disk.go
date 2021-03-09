0c8da0ff4fde7cfdc1d229872d4fe246e81eb7a2
지농 <jinhong0719@naver.com>
2021-03-09 19:38:43 +0900

    [CREATE] <domain/syscheck_disk.go> Create file about disk in syscheck domain

---
+++ domain/syscheck_disk.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// syscheck_disk.go is file that declare model struct & repo interface about disk in syscheck domain.
+// also, additional method of model struct is declared in this file, too.
+
+package domain
