7c39789e44b7ced1f91756f6d3307a548d1e2660
지농 <jinhong0719@naver.com>
2021-03-10 10:19:39 +0900

    [ADD] <syscheck/usecase/syscheck_disk_ucase.go> Add diskCheckUsecase struct

--- syscheck/usecase/syscheck_disk_ucase.go
+++ syscheck/usecase/syscheck_disk_ucase.go
@@ -1,5 +1,16 @@
 // Create file in v.1.0.0
 // syscheck_disk_ucase.go is file that define usecase implementation about disk check domain
-// 
+// disk check usecase struct embed systemCheckUsecaseComponent struct in ./syscheck.go file
 
 package usecase
+
+import "github.com/DMS-SMS/v1-health-check/domain"
+
+// diskCheckUsecase implement DiskCheckUsecase interface in domain and used in delivery layer
+type diskCheckUsecase struct {
+	// get required component by embedding systemCheckUsecaseComponent struct
+	systemCheckUsecaseComponent
+
+	// historyRepo is used for store disk check history and injected from outside
+	historyRepo domain.DiskCheckHistoryRepository
+}
