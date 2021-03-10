a1456aa20df6c39909ff2f2bab574ee61d223b30
지농 <jinhong0719@naver.com>
2021-03-10 09:55:28 +0900

    [CREATE] <syscheck/usecase/syscheck.go> Create file & Add file, package comment

---
+++ syscheck/usecase/syscheck.go
@@ -0,0 +1,9 @@
+// Create package in v.1.0.0
+// usecase package declare implementation of usecase interface about syscheck domain
+// all usecase implementation will accept any input from Delivery layer
+// This usecase layer will depends to Repository layer
+
+// syscheck.go is file that define structure to embed from another structures.
+// It also defines variables or constants used jointly in the package as private.
+
+package usecase
