5bc89c3f79b622c402f491291b8c7d1a22f92000
지농 <jinhong0719@naver.com>
2021-03-11 19:15:10 +0900

    [REMOVE] <config/config.go> Remove config directory & file and package

--- config/config.go
+++
@@ -1,8 +0,0 @@
-// Create file in v.1.0.0.
-// config package contains App global variable with config value from environment variable or config file
-// App return field value from method having same name with that field name
-
-// config.go is file that define Config type which is type of App
-// Also, App implement various Config interface each of package by declaring it's method
-
-package config
