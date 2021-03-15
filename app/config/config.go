16b5fe2b9b681c9125227149531767703699066b
지농 <jinhong0719@naver.com>
2021-03-15 20:12:24 +0900

    [CREATE] <app/config/config.go> Create file to contain config using in app(main)

---
+++ app/config/config.go
@@ -0,0 +1,9 @@
+// Create package in v.1.0.0
+// config package contains App global variable with config value using in app(main) package from environment variable or config file
+// App return field value from method having same name with that field name
+
+// config.go is file that define appConfig type which is type of App
+// appConfig dose not implement any interface, so use by explicitly importing in app(main) package
+// Also, App implement various config interface each of package in syscheck domain by declaring method
+
+package config
