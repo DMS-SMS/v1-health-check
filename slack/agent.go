5f91124a2b27787bdf053e35029237c66dd9a18a
지농 <jinhong0719@naver.com>
2021-03-18 14:10:59 +0900

    [CREATE] <slack/agent.go> Create file & Add comment about file and package

---
+++ slack/agent.go
@@ -0,0 +1,8 @@
+// Create package in v.1.0.0
+// slack package define struct which is implement various interface about slack agent using in each of domain
+// there are kind of slack agent such as chat, conversations, admin, etc.
+
+// in agent.go file, define struct type of slack agent & initializer that are not method.
+// Also if exist, custom type or variable used in common in each of method will declared in this file.
+
+package slack
