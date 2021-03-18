532dbad268d229e2c41c783ec4818c328d1823a1
지농 <jinhong0719@naver.com>
2021-03-18 14:55:04 +0900

    [CREATE] <slack/agent_chat.go> Create file & Add file description comment

---
+++ slack/agent_chat.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// agent_chat.go file define method of slackAgent about slack chat API
+// implement agency interface about slack chat defined in each of domain
+
+package slack
