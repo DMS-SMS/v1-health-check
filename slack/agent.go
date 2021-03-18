fddbf508f5bfc709ed0b37069837fae8b192ea42
지농 <jinhong0719@naver.com>
2021-03-18 14:37:51 +0900

    [ADD] <slack/agent.go> Add slackAgent type and constructor & Add comments

--- slack/agent.go
+++ slack/agent.go
@@ -1,8 +1,23 @@
 // Create package in v.1.0.0
-// slack package define struct which is implement various interface about slack agent using in each of domain
-// there are kind of slack agent such as chat, conversations, admin, etc.
+// slack package define struct which is implement various interface about slack agency using in each of domain
+// there are kind of slack agency such as chat, conversations, admin, etc.
 
 // in agent.go file, define struct type of slack agent & initializer that are not method.
 // Also if exist, custom type or variable used in common in each of method will declared in this file.
 
 package slack
+
+import "github.com/slack-go/slack"
+
+// slackAgent agent various slack API(chat, conversations, admin, etc ...) as implementation
+type slackAgent struct {
+	// slkCli is slack client connection injected from the outside package
+	slkCli *slack.Client
+}
+
+// NewAgent return new initialized instance of slackAgent pointer type with slack API token & options
+func NewAgent(token string, opts ...slack.Option) *slackAgent {
+	return &slackAgent{
+		slkCli: slack.New(token, opts...),
+	}
+}
