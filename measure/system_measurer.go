97db0115765f34f327e13426474ff9e8453d87f3
지농 <jinhong0719@naver.com>
2021-03-21 23:16:51 +0900

    [ADD] <measure/system_measurer.go> Add systemMeasurer struct & initializer

--- measure/system_measurer.go
+++ measure/system_measurer.go
@@ -3,3 +3,11 @@
 // system_measurer.go define system measurer about cpu or memory usage, disk remain capacity, etc ...
 
 package measure
+
+// systemMeasurer is struct that measure value about system
+type systemMeasurer struct {}
+
+// SystemMeasurer function return systemMeasurer ptr instance with initializing
+func SystemMeasurer() *systemMeasurer {
+	return &systemMeasurer{}
+}
