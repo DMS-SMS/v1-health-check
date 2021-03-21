bcd3133e0144dcba4272c8227dccd46fdd5ad2d8
지농 <jinhong0719@naver.com>
2021-03-21 17:13:12 +0900

    [ADD] <syscheck/usecase/syscheck_cpu_ucase.go> Add cpuCheckUsecaseConfig

--- syscheck/usecase/syscheck_cpu_ucase.go
+++ syscheck/usecase/syscheck_cpu_ucase.go
@@ -11,3 +11,12 @@ const (
 	cpuStatusRecovering                       // represent it's recovering cpu status now
 	cpuStatusUnhealthy                        // represent cpu check status is unhealthy
 )
+
+// cpuCheckUsecaseConfig is the config getter interface for cpu check usecase
+type cpuCheckUsecaseConfig interface {
+	// get common config method from embedding systemCheckUsecaseComponentConfig
+	systemCheckUsecaseComponentConfig
+
+	// CPUUsageLimit method returns float32 represent cpu usage limit value
+	CPUUsageLimit() float32
+}
