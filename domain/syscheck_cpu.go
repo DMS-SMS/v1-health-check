ad1c8c682dc09e1736e6ae7d03bd87c2f7f980ef
지농 <jinhong0719@naver.com>
2021-03-21 15:42:22 +0900

    [ADD] <domain/syscheck_cpu.go> Add CPUCheckHistory struct & Add description

--- domain/syscheck_cpu.go
+++ domain/syscheck_cpu.go
@@ -3,3 +3,17 @@
 // also, additional method of model struct is declared in this file, too.
 
 package domain
+
+import "github.com/inhies/go-bytesize"
+
+// CPUCheckHistory model is used for record cpu health check history and result
+type CPUCheckHistory struct {
+	// get required component by embedding systemCheckHistoryComponent
+	systemCheckHistoryComponent
+
+	// Usage specifies current cpu usage of runtime system looked in cpu check
+	Usage bytesize.ByteSize
+
+	// Free specifies freed cpu size while recovering cpu health
+	Free bytesize.ByteSize
+}
