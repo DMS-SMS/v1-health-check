29b7d26bc3745c4a9227c75d2dcfe0477e24d42c
지농 <jinhong0719@naver.com>
2021-03-21 16:45:18 +0900

    [ADD] <../elasticsearch/syscheck_cpu_repo.go> Add esCPUCheckHistoryRepository

--- syscheck/repository/elasticsearch/syscheck_cpu_repo.go
+++ syscheck/repository/elasticsearch/syscheck_cpu_repo.go
@@ -4,6 +4,20 @@
 
 package elasticsearch
 
+import "github.com/elastic/go-elasticsearch/v7"
+
+// esCPUCheckHistoryRepository is to handle CPUCheckHistory model using elasticsearch as data store
+type esCPUCheckHistoryRepository struct {
+	// myCfg is used for get cpu check history repository config about elasticsearch
+	myCfg esCPUCheckHistoryRepoConfig
+
+	// esCli is elasticsearch client connection injected from the outside package
+	esCli *elasticsearch.Client
+
+	// bodyWriter is implementation of reqBodyWriter interface to write []byte for request body
+	bodyWriter reqBodyWriter
+}
+
 // esCPUCheckHistoryRepoConfig is the config for cpu check history repository using elasticsearch
 type esCPUCheckHistoryRepoConfig interface {
 	// get common method from embedding esRepositoryComponentConfig
