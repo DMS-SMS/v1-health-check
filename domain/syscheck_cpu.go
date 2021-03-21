a24bdb18ba79f07c282d7092673de739c5ade15a
지농 <jinhong0719@naver.com>
2021-03-21 15:18:10 +0900

    [ADD] <domain/syscheck_cpu.go> Add comment about file description

--- domain/syscheck_cpu.go
+++ domain/syscheck_cpu.go
@@ -1,1 +1,5 @@
+// Create file in v.1.0.0
+// syscheck_cpu.go is file that declare model struct & repo interface about cpu health check in syscheck domain.
+// also, additional method of model struct is declared in this file, too.
+
 package domain
