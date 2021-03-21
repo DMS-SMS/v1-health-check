fa6741203cf7779d54f941700c931384a3cc9d6f
지농 <jinhong0719@naver.com>
2021-03-21 16:38:54 +0900

    [CREATE] <../elasticsearch/syscheck_cpu_repo.go> Create file & Add description

---
+++ syscheck/repository/elasticsearch/syscheck_cpu_repo.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// syscheck_cpu_repo.go is file that define implement cpu history repository using elasticsearch
+// this cpu repository struct embed esRepositoryRequiredComponent struct in ./syscheck.go file
+
+package elasticsearch
