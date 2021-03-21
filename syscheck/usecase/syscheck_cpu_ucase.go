865e94063f493e47046a11a80ff8ea67e1a39018
지농 <jinhong0719@naver.com>
2021-03-21 17:03:11 +0900

    [CREATE] <usecase/syscheck_cpu_ucase.go> Create file & Add description

---
+++ syscheck/usecase/syscheck_cpu_ucase.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// syscheck_cpu_ucase.go is file that define usecase implementation about syscheck cpu domain
+// cpu check usecase struct embed systemCheckUsecaseComponent struct in ./syscheck.go file
+
+package usecase
