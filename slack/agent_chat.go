afbb8a44dc31c452fdb584b29318006f115e8f24
지농 <jinhong0719@naver.com>
2021-03-21 14:05:42 +0900

    [MODIFY] <slack/agent_chat.go> Change SendMessage signature & logic of slackAgent

--- slack/agent_chat.go
+++ slack/agent_chat.go
@@ -12,13 +12,13 @@ import (
 	"time"
 )
 
-// SendMessage send message with text & emoji using slack API and return send time & error
-func (sa *slackAgent) SendMessage(emoji, text string, opts ...slack.MsgOption) (t time.Time, err error) {
+// SendMessage send message with text & emoji using slack API and return send time & text & error
+func (sa *slackAgent) SendMessage(emoji, text, uuid string, opts ...slack.MsgOption) (t time.Time, _text string, err error) {
 	if emoji != "" {
-		text = fmt.Sprintf(":%s: %s", emoji, text)
+		_text = fmt.Sprintf(":%s: %s (%s)", emoji, text, uuid)
 	}
 
-	opts = append(opts, slack.MsgOptionText(text, false))
+	opts = append(opts, slack.MsgOptionText(_text, false))
 	_, _time, _, err := sa.slkCli.SendMessage(sa.chatChannel, opts...)
 	if err != nil {
 		err = errors.Wrap(err, "failed to send message with slack API")
