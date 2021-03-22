fe6f7a8ba543a28ee59e405f4287c669cb16279a
지농 <jinhong0719@naver.com>
2021-03-22 09:04:58 +0900

    [CREATE] <sysagent/agent.go> Create file & Add description about file and package

---
+++ sysagent/agent.go
@@ -0,0 +1,8 @@
+// Create package in v.1.0.0
+// sysagent package define system agency that agent various command about system
+// For example about disk system, agent get remaining disk capacity, prune disk with docker, etc ...
+
+// in agent.go file, define agent struct & initializer
+// this agent implement various interface about system agency defined and using in usecase layer of syscheck domain
+
+package sysagent
