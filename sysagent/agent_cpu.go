21a5f740cd7ef7dce2846002314267aa5843952e
지농 <jinhong0719@naver.com>
2021-03-22 10:08:17 +0900

    [CREATE] <sysagent/agent_cpu.go> Create file & Add description about file

---
+++ sysagent/agent_cpu.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// agent_cpu.go is file that define method of sysAgent that agent command about cpu
+// For example in cpu command, there are get total cpu usage, prune cpu, etc ...
+
+package sysagent
