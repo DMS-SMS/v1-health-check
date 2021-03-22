216f9dd16f05502c182d73501369eb68c883027a
지농 <jinhong0719@naver.com>
2021-03-22 09:15:14 +0900

    [ADD] <sysagent/agent_disk.go> Add GetRemainDiskCapacity method in sysAgent

--- sysagent/agent_disk.go
+++ sysagent/agent_disk.go
@@ -3,3 +3,30 @@
 // For example in disk command, there are get remaining capacity, prune disk, etc ...
 
 package sysagent
+
+import (
+	"github.com/inhies/go-bytesize"
+	"github.com/pkg/errors"
+	"golang.org/x/sys/unix"
+	"os"
+)
+
+// GetRemainDiskCapacity return remain disk capacity expressed in bytesize package
+func (sa *sysAgent) GetRemainDiskCapacity() (size bytesize.ByteSize, err error) {
+	var stat unix.Statfs_t
+
+	wd, err := os.Getwd()
+	if err != nil {
+		err = errors.Wrap(err, "failed to call os.Getwd")
+		return
+	}
+
+	if err = unix.Statfs(wd, &stat); err != nil {
+		err = errors.Wrap(err, "failed to call unix.Statfs")
+		return
+	}
+
+	// Available blocks * size per block = available space in bytes
+	size = bytesize.New(float64(stat.Bavail * uint64(stat.Bsize)))
+	return
+}
