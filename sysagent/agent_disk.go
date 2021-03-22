643d2f271474950fb07d71db18aa9de46c8798d1
지농 <jinhong0719@naver.com>
2021-03-22 09:13:31 +0900

    [CREATE] <sysagent/agent_disk.go> Create file & Add description about file

---
+++ sysagent/agent_disk.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// agent_disk.go is file that define method of sysAgent that agent command about disk
+// For example in disk command, there are get remaining capacity, prune disk, etc ...
+
+package sysagent
