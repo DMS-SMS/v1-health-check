9b66193fa5609cc7e801e6e9669ff44c0c3a4b00
지농 <jinhong0719@naver.com>
2021-03-23 19:30:47 +0900

    [ADD] <agent/docker/agent_container.go> Add RemoveContainer method

--- agent/docker/agent_container.go
+++ agent/docker/agent_container.go
@@ -3,3 +3,18 @@
 // For example in disk command, there are remove or restart container, etc ...
 
 package docker
+
+import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/pkg/errors"
+)
+
+// RemoveContainer remove container with id & option (auto created from docker swarm if exists)
+func (da *dockerAgent) RemoveContainer(containerID string, options types.ContainerRemoveOptions) error {
+	var (
+		ctx = context.Background()
+	)
+
+	return errors.Wrap(da.dkrCli.ContainerRemove(ctx, containerID, options), "failed to call ContainerRemove")
+}
