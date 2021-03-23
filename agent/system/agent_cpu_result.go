702f683cf0899bc8949995893fb3751633153b9b
지농 <jinhong0719@naver.com>
2021-03-23 11:34:19 +0900

    [ADD] <agent/system/agent_cpu_result.go> Move calculateContainersCPUUsageResult

--- agent/system/agent_cpu_result.go
+++ agent/system/agent_cpu_result.go
@@ -3,3 +3,27 @@
 // all result struct implement interface defined in return type of method signature in agent_cpu.go
 
 package system
+
+import "runtime"
+
+// calculateContainersCPUUsageResult is result type of CalculateContainersCPUUsage
+type calculateContainersCPUUsageResult struct {
+	// cpuNum represent number of cpu core
+	cpuNum int
+
+	// containers is to keep cpu usage each of container get from GetTotalCPUUsage
+	containers []struct {
+		id, name   string
+		cpuPercent float64
+	}
+}
+
+// TotalCPUUsage return total cpu usage in docker containers
+func (result calculateContainersCPUUsageResult) TotalCPUUsage() (usage float64) {
+	var percent float64 = 0
+	for _, container := range result.containers {
+		percent += container.cpuPercent
+	}
+	usage = float64(runtime.NumCPU()) / 100 * percent
+	return
+}
