3f89c97caa00419e77d9a28daa0125bb1f7ebc60
지농 <jinhong0719@naver.com>
2021-03-23 19:27:01 +0900

    [CREATE] <agent/docker/agent_container.go> Create file & Add file description

---
+++ agent/docker/agent_container.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// agent_container.go is file that define method of dockerAgent that agent command about docker container
+// For example in disk command, there are remove or restart container, etc ...
+
+package docker
