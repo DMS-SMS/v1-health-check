f7587d34e5e0041cee84abe559c8914c722625cf
지농 <jinhong0719@naver.com>
2021-03-23 19:26:46 +0900

    [ADD] <agent/docker/agent.go> Add dockerAgent struct & Add New initializer

--- agent/docker/agent.go
+++ agent/docker/agent.go
@@ -6,3 +6,17 @@
 // Also if exist, custom type or variable used in common in each of method will declared in this file.
 
 package docker
+
+import "github.com/docker/docker/client"
+
+// dockerAgent is struct that agent various command about docker including container, service, etc ...
+type dockerAgent struct {
+	dkrCli *client.Client
+}
+
+// New return new instance of dockerAgent pointer type initialized with parameter
+func New(dc *client.Client) *dockerAgent {
+	return &dockerAgent{
+		dkrCli: dc,
+	}
+}
