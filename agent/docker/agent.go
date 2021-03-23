c623ffa1957d07e0eba718fb2f5c1939ecc09450
지농 <jinhong0719@naver.com>
2021-03-23 19:14:25 +0900

    [CREATE] <agent/docker/agent.go> Create package & Add file, package comment

---
+++ agent/docker/agent.go
@@ -0,0 +1,8 @@
+// Create package in v.1.0.0
+// docker package define struct which is implement various interface about docker agency using in each of domain
+// there are kind of method in docker agency such as container, service, etc ...
+
+// in agent.go file, define struct type of docker agent & initializer that are not method.
+// Also if exist, custom type or variable used in common in each of method will declared in this file.
+
+package docker
