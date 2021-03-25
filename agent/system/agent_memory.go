6aaab78e7cc40825c8f8397b33ff51d3d26b39f4
지농 <jinhong0719@naver.com>
2021-03-25 16:03:57 +0900

    [CREATE] <agent/system/agent_memory.go> Create file & Add file description

---
+++ agent/system/agent_memory.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// agent_memory.go is file that define method of sysAgent that agent command about memory
+// For example in memory command, there are get total memory usage, calculate container memory usage, etc ...
+
+package system
