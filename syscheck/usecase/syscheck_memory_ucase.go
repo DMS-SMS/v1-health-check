bcfdfc0f5e8ae0ee75b46e0a49a131cab7415ba3
지농 <jinhong0719@naver.com>
2021-03-25 14:37:25 +0900

    [ADD] <syscheck/usecase/syscheck_memory_ucase.go> Add memoryCheckStatus & instance

--- syscheck/usecase/syscheck_memory_ucase.go
+++ syscheck/usecase/syscheck_memory_ucase.go
@@ -1,5 +1,14 @@
 // Create file in v.1.0.0
-// syscheck_cpu_ucase.go is file that define usecase implementation about syscheck cpu domain
-// cpu check usecase struct embed systemCheckUsecaseComponent struct in ./syscheck.go file
+// syscheck_memory_ucase.go is file that define usecase implementation about syscheck memory domain
+// memory check usecase struct embed systemCheckUsecaseComponent struct in ./syscheck.go file
 
 package usecase
+
+// memoryCheckStatus is type to int constant represent current memory check process status
+type memoryCheckStatus int
+const (
+	memoryStatusHealthy    memoryCheckStatus = iota // represent memory check status is healthy
+	memoryStatusWarning                             // represent memory check status is warning now
+	memoryStatusRecovering                          // represent it's recovering memory status now
+	memoryStatusUnhealthy                           // represent memory check status is unhealthy
+)
