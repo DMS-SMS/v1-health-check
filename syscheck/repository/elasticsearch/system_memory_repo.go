f18ee1dbe8b020a0aa7beba7d720801e86b15e5f
지농 <jinhong0719@naver.com>
2021-03-25 14:14:37 +0900

    [ADD] <../elasticsearch/system_memory_repo.go> Add NewESMemoryCheckHistoryRepository

--- syscheck/repository/elasticsearch/system_memory_repo.go
+++ syscheck/repository/elasticsearch/system_memory_repo.go
@@ -1,14 +1,20 @@
 // Create file in v.1.0.0
 // syscheck_memory_repo.go is file that define repository implement about memory using elasticsearch
-// disk repository struct embed esRepositoryRequiredComponent struct in ./syscheck.go file
+// memory repository struct embed esRepositoryRequiredComponent struct in ./syscheck.go file
 
 package elasticsearch
 
-import "github.com/elastic/go-elasticsearch/v7"
+import (
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/pkg/errors"
+	"log"
+
+	"github.com/DMS-SMS/v1-health-check/domain"
+)
 
 // esMemoryCheckHistoryRepository is to handle MemoryCheckHistory model using elasticsearch as data store
 type esMemoryCheckHistoryRepository struct {
-	// myCfg is used for get disk check history repository config about elasticsearch
+	// myCfg is used for get memory check history repository config about elasticsearch
 	myCfg esMemoryCheckHistoryRepoConfig
 
 	// esCli is elasticsearch client connection injected from the outside package
@@ -22,4 +28,19 @@ type esMemoryCheckHistoryRepository struct {
 type esMemoryCheckHistoryRepoConfig interface {
 	// get common method from embedding esRepositoryComponentConfig
 	esRepositoryComponentConfig
+}
+
+// NewESMemoryCheckHistoryRepository return new object that implement MemoryCheckHistoryRepository interface
+func NewESMemoryCheckHistoryRepository(cfg esMemoryCheckHistoryRepoConfig, cli *elasticsearch.Client, w reqBodyWriter) domain.MemoryCheckHistoryRepository {
+	repo := &esMemoryCheckHistoryRepository{
+		myCfg:      cfg,
+		esCli:      cli,
+		bodyWriter: w,
+	}
+
+	if err := repo.Migrate(); err != nil {
+		log.Fatal(errors.Wrap(err, "could not migrate repository").Error())
+	}
+
+	return repo
 }
