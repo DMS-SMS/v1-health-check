9e350da0c8bb0cc64fd020bc81486d0835ce0f4a
지농 <jinhong0719@naver.com>
2021-03-25 14:10:44 +0900

    [ADD] <../elasticsearch/system_memory_repo.go> Add esMemoryCheckHistoryRepository

--- syscheck/repository/elasticsearch/system_memory_repo.go
+++ syscheck/repository/elasticsearch/system_memory_repo.go
@@ -3,3 +3,17 @@
 // disk repository struct embed esRepositoryRequiredComponent struct in ./syscheck.go file
 
 package elasticsearch
+
+import "github.com/elastic/go-elasticsearch/v7"
+
+// esMemoryCheckHistoryRepository is to handle MemoryCheckHistory model using elasticsearch as data store
+type esMemoryCheckHistoryRepository struct {
+	// myCfg is used for get disk check history repository config about elasticsearch
+	myCfg esMemoryCheckHistoryRepoConfig
+
+	// esCli is elasticsearch client connection injected from the outside package
+	esCli *elasticsearch.Client
+
+	// bodyWriter is implementation of reqBodyWriter interface to write []byte for request body
+	bodyWriter reqBodyWriter
+}
