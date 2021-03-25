dd8ea5e5c819aad6956fc2dd92e849f85b7e1f09
지농 <jinhong0719@naver.com>
2021-03-26 08:53:02 +0900

    [ADD] <agent/system/agent_memory.go> Add GetTotalSystemMemoryUsage method

--- agent/system/agent_memory.go
+++ agent/system/agent_memory.go
@@ -3,3 +3,21 @@
 // For example in memory command, there are get total memory usage, calculate container memory usage, etc ...
 
 package system
+
+import (
+	"github.com/inhies/go-bytesize"
+	"github.com/mackerelio/go-osstat/memory"
+	"github.com/pkg/errors"
+)
+
+// GetTotalSystemMemoryUsage return total memory usage as bytesize in system
+func (sa *sysAgent) GetTotalSystemMemoryUsage() (size bytesize.ByteSize, err error) {
+	stats, err := memory.Get()
+	if err != nil {
+		err = errors.Wrap(err, "failed to get memory stats")
+		return
+	}
+
+	size = bytesize.ByteSize(stats.Used)
+	return
+}
