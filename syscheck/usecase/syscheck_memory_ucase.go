7ccac6869f77b5d2d6bb9871c0d80634057111e2
지농 <jinhong0719@naver.com>
2021-03-25 14:40:26 +0900

    [ADD] <syscheck/usecase/syscheck_memory_ucase.go> Add memoryCheckUsecase struct

--- syscheck/usecase/syscheck_memory_ucase.go
+++ syscheck/usecase/syscheck_memory_ucase.go
@@ -4,6 +4,12 @@
 
 package usecase
 
+import (
+	"sync"
+
+	"github.com/DMS-SMS/v1-health-check/domain"
+)
+
 // memoryCheckStatus is type to int constant represent current memory check process status
 type memoryCheckStatus int
 const (
@@ -12,3 +18,27 @@ const (
 	memoryStatusRecovering                          // represent it's recovering memory status now
 	memoryStatusUnhealthy                           // represent memory check status is unhealthy
 )
+
+// memoryCheckUsecase implement MemoryCheckUsecase interface in domain and used in delivery layer
+type memoryCheckUsecase struct {
+	//// myCfg is used for getting memory check usecase config
+	//myCfg memoryCheckUsecaseConfig
+
+	// historyRepo is used for store memory check history and injected from outside
+	historyRepo domain.MemoryCheckHistoryRepository
+
+	// slackChat is used for agent slack API about chatting
+	slackChatAgency slackChatAgency
+
+	//// memorySysAgency is used as agency about memory system command
+	//memorySysAgency memorySysAgency
+
+	// dockerAgency is used as agency about docker command
+	dockerAgency dockerAgency
+
+	// status represent current process status of memory health check
+	status memoryCheckStatus
+
+	// mutex help to prevent race condition when set status field value
+	mutex sync.Mutex
+}
