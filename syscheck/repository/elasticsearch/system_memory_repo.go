4e54c6b445cde8bd61daf0d430668cf9ffc01853
지농 <jinhong0719@naver.com>
2021-03-25 14:09:03 +0900

    [ADD] <../elasticsearch/system_memory_repo.go> Create file & Add description

---
+++ syscheck/repository/elasticsearch/system_memory_repo.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// syscheck_memory_repo.go is file that define repository implement about memory using elasticsearch
+// disk repository struct embed esRepositoryRequiredComponent struct in ./syscheck.go file
+
+package elasticsearch
