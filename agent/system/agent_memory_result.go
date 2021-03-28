3b3e15b2fc50d347652c4ec80db9a13aae0d4695
지농 <jinhong0719@naver.com>
2021-03-28 22:57:45 +0900

    [CREATE] <agent/system/agent_memory_result.go> Create file & Add description

---
+++ agent/system/agent_memory_result.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// agent_memory_result.go is file that define result struct using as return value in agent method about memory
+// all result struct implement interface defined in return type of method signature in agent_memory.go
+
+package system
