b5816dfe7b7a3728d6467d046ee4feb040fb4328
지농 <jinhong0719@naver.com>
2021-03-28 23:04:35 +0900

    [ADD] <agent/system/agent_memory_result.go> Add calculateContainersMemoryUsageResult

--- agent/system/agent_memory_result.go
+++ agent/system/agent_memory_result.go
@@ -3,3 +3,14 @@
 // all result struct implement interface defined in return type of method signature in agent_memory.go
 
 package system
+
+import "github.com/inhies/go-bytesize"
+
+// calculateContainersMemoryUsageResult is result type of CalculateContainersMemoryUsage
+type calculateContainersMemoryUsageResult struct {
+	// containers is to keep memory usage each of container get from CalculateContainersMemoryUsage
+	containers []struct {
+		id, name string
+		usage    bytesize.ByteSize
+	}
+}
