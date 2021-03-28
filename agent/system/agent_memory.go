731e735999d7293a89616355913a6b3bdbac74db
지농 <jinhong0719@naver.com>
2021-03-28 23:17:23 +0900

    [ADD] <agent/system/agent_memory.go> Add CalculateContainersMemoryUsage method

--- agent/system/agent_memory.go
+++ agent/system/agent_memory.go
@@ -5,19 +5,65 @@
 package system
 
 import (
+	"context"
+	"encoding/json"
+	"github.com/docker/docker/api/types"
 	"github.com/inhies/go-bytesize"
 	"github.com/mackerelio/go-osstat/memory"
 	"github.com/pkg/errors"
 )
 
 // GetTotalSystemMemoryUsage return total memory usage as bytesize in system
-func (sa *sysAgent) GetTotalSystemMemoryUsage() (size bytesize.ByteSize, err error) {
+func (sa *sysAgent) GetTotalSystemMemoryUsage() (usage bytesize.ByteSize, err error) {
 	stats, err := memory.Get()
 	if err != nil {
 		err = errors.Wrap(err, "failed to get memory stats")
 		return
 	}
 
-	size = bytesize.ByteSize(stats.Used)
+	usage = bytesize.ByteSize(stats.Used)
 	return
+}
+
+// CalculateContainersCPUUsage calculate memory usage & return calculateContainersMemoryUsageResult
+func (sa *sysAgent) CalculateContainersMemoryUsage() (interface {
+	TotalMemoryUsage() (usage bytesize.ByteSize)
+	MostConsumerExceptFor(names []string) (id, name string, usage bytesize.ByteSize)
+}, error) {
+	var (
+		ctx    = context.Background()
+		result = calculateContainersMemoryUsageResult{}
+	)
+
+	containers, err := sa.dockerCli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get container list from docker")
+	}
+
+	result.containers = make([]struct {
+		id, name string
+		usage    bytesize.ByteSize
+	}, len(containers))
+
+	for i, container := range containers {
+		var stats types.ContainerStats
+		if stats, err = sa.dockerCli.ContainerStats(ctx, container.ID, false); err != nil {
+			return nil, errors.Wrap(err, "failed to get container stats from docker")
+		}
+
+		v := &types.StatsJSON{}
+		if err = json.NewDecoder(stats.Body).Decode(v); err != nil {
+			return nil, errors.Wrap(err, "failed to decode stats response body to struct")
+		}
+
+		result.containers[i] = struct {
+			id, name string
+			usage    bytesize.ByteSize
+		}{
+			id: v.ID, name: v.Name,
+			usage: getMemoryUsageSizeFrom(v),
+		}
+	}
+
+	return nil, nil
 }
