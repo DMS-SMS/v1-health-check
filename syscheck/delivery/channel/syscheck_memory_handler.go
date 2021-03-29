87d57ea81f7a43f931585f61e7eddee30563d026
지농 <jinhong0719@naver.com>
2021-03-29 17:16:54 +0900

    [CREATE] <syscheck/delivery/channel/syscheck_memory_handler.go> Create file

---
+++ syscheck/delivery/channel/syscheck_memory_handler.go
@@ -0,0 +1,4 @@
+// in syscheck_memory_handler.go file, define delivery from channel msg to memory usecase handler
+// publishing msg to golang channel which is received from outside is not occurred in this package
+
+package channel
