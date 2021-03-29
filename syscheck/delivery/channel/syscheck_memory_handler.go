c76e8ac582f225ec88d052c14b41b82add332b07
지농 <jinhong0719@naver.com>
2021-03-29 17:17:31 +0900

    [CREATE] <syscheck/delivery/channel/syscheck_memory_handler.go> Add method

--- syscheck/delivery/channel/syscheck_memory_handler.go
+++ syscheck/delivery/channel/syscheck_memory_handler.go
@@ -4,6 +4,9 @@
 package channel
 
 import (
+	"context"
+	"log"
+	"time"
 
 	"github.com/DMS-SMS/v1-health-check/domain"
 )
@@ -14,3 +17,23 @@ type memoryCheckHandler struct {
 	MUsecase domain.MemoryCheckUseCase
 }
 
+
+// CheckMemory method set context & call usecase CheckMemory method, handle error
+func (mh *memoryCheckHandler) CheckMemory(t time.Time) {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "time", t)
+
+	if err := mh.MUsecase.CheckMemory(ctx); err != nil {
+		log.Printf("error occurs in CheckMemory, err: %v", err)
+	}
+}
+
+// startListening method start listening msg from golang channel & stream msg to another method
+func (mh *memoryCheckHandler) startListening(c <-chan time.Time) {
+	for {
+		select {
+		case t := <-c:
+			mh.CheckMemory(t)
+		}
+	}
+}
