287a5edd2d05a55be550168f43b6fd119e688dc1
지농 <jinhong0719@naver.com>
2021-03-29 17:17:16 +0900

    [CREATE] <syscheck/delivery/channel/syscheck_memory_handler.go> Add memoryCheckHandler

--- syscheck/delivery/channel/syscheck_memory_handler.go
+++ syscheck/delivery/channel/syscheck_memory_handler.go
@@ -2,3 +2,15 @@
 // publishing msg to golang channel which is received from outside is not occurred in this package
 
 package channel
+
+import (
+
+	"github.com/DMS-SMS/v1-health-check/domain"
+)
+
+// memoryCheckHandler is delivered data handler about memory check using usecase layer
+type memoryCheckHandler struct {
+	// CUsecase is usecase layer interface which is injected from package outside (maybe, in main)
+	MUsecase domain.MemoryCheckUseCase
+}
+
