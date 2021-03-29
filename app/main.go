3522b93a0d2c0d73a1cb57678845b001411e718b
지농 <jinhong0719@naver.com>
2021-03-29 19:04:33 +0900

    [ADD] <app/main.go> Add repository & usecase & delivery of syscheck domain

--- app/main.go
+++ app/main.go
@@ -6,10 +6,21 @@ package main
 import (
 	"github.com/docker/docker/client"
 	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/slack-go/slack"
 	"github.com/spf13/viper"
 	"log"
+	"runtime"
+	"time"
 
+	dockeragent "github.com/DMS-SMS/v1-health-check/agent/docker"
+	slackagent "github.com/DMS-SMS/v1-health-check/agent/slack"
+	sysagent "github.com/DMS-SMS/v1-health-check/agent/system"
 	"github.com/DMS-SMS/v1-health-check/app/config"
+	"github.com/DMS-SMS/v1-health-check/json"
+	_syscheckConfig "github.com/DMS-SMS/v1-health-check/syscheck/config"
+	_syscheckChannelDelivery "github.com/DMS-SMS/v1-health-check/syscheck/delivery/channel"
+	_syscheckRepo "github.com/DMS-SMS/v1-health-check/syscheck/repository/elasticsearch"
+	_syscheckUcase "github.com/DMS-SMS/v1-health-check/syscheck/usecase"
 )
 
 func init() {
@@ -30,9 +41,31 @@ func main() {
 	})
 	if err != nil {
 		log.Fatal(err)
-}
+	}
 
 	dkrCli, err := client.NewClientWithOpts(
 		client.WithVersion(config.App.DockerCliVer()),
 		client.WithTimeout(config.App.DockerCliTimeout()),
 	)
+
+	_slk := slackagent.New(slack.New(config.App.SlackAPIToken()), config.App.SlackChatChannel())
+	_sys := sysagent.New(dkrCli)
+	_dkr := dockeragent.New(dkrCli)
+
+	// syscheck domain repository
+	sdr := _syscheckRepo.NewESDiskCheckHistoryRepository(_syscheckConfig.App, esCli, json.MapWriter())
+	scr := _syscheckRepo.NewESCPUCheckHistoryRepository(_syscheckConfig.App, esCli, json.MapWriter())
+	smr := _syscheckRepo.NewESMemoryCheckHistoryRepository(_syscheckConfig.App, esCli, json.MapWriter())
+
+	// syscheck domain usecase
+	sdu := _syscheckUcase.NewDiskCheckUsecase(_syscheckConfig.App, sdr, _slk, _sys)
+	scu := _syscheckUcase.NewCPUCheckUsecase(_syscheckConfig.App, scr, _slk, _sys, _dkr)
+	smu := _syscheckUcase.NewMemoryCheckUsecase(_syscheckConfig.App, smr, _slk, _sys, _dkr)
+
+	// syscheck domain delivery
+	_syscheckChannelDelivery.NewDiskCheckHandler(time.Tick(time.Minute * 5), sdu)
+	_syscheckChannelDelivery.NewCPUCheckHandler(time.Tick(time.Minute * 5), scu)
+	_syscheckChannelDelivery.NewMemoryCheckHandler(time.Tick(time.Minute * 5), smu)
+
+	runtime.Goexit()
+}
