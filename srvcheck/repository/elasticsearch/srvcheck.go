998a366abaf8152730f7efd59c68c572a6d6a83c
지농 <jinhong0719@naver.com>
2021-03-30 20:20:17 +0900

    [CREATE] <srvcheck/repository/elasticsearch/srvcheck.go> Create file & Add comment

---
+++ srvcheck/repository/elasticsearch/srvcheck.go
@@ -0,0 +1,8 @@
+// Create package in v.1.0.0
+// elasticsearch package is for implementations of srvcheck domain repository using elasticsearch
+// In practice, repository struct declaration and implementation occur in this package
+
+// srvcheck.go is file that define structure to embed from another structures.
+// It also defines interface or function used jointly in the package as private.
+
+package elasticsearch
