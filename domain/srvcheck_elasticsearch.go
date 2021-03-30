396938182a8096d38be53eaebf0c959aa47013b9
지농 <jinhong0719@naver.com>
2021-03-30 19:37:03 +0900

    [CREATE] <domain/srvcheck_elasticsearch.go> Create file & Add file description

---
+++ domain/srvcheck_elasticsearch.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// syscheck_elasticsearch.go is file that declare model struct & repo interface about elasticsearch check in srvcheck domain.
+// also, additional method of model struct is declared in this file, too.
+
+package domain
