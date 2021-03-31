7cb55d3a4deca55db19b0819c26e3e600579c67f
지농 <jinhong0719@naver.com>
2021-03-31 10:21:43 +0900

    [ADD] <../elasticsearch/srvcheck_elasticsearch_repo.go> Add repository struct

--- srvcheck/repository/elasticsearch/srvcheck_elasticsearch_repo.go
+++ srvcheck/repository/elasticsearch/srvcheck_elasticsearch_repo.go
@@ -3,3 +3,17 @@
 // this elasticsearch repository struct embed esRepositoryRequiredComponent struct in ./srvcheck.go file
 
 package elasticsearch
+
+import "github.com/elastic/go-elasticsearch/v7"
+
+// esElasticsearchCheckHistoryRepository is to handle ElasticsearchCheckHistoryRepository model using elasticsearch as data store
+type esElasticsearchCheckHistoryRepository struct {
+	// myCfg is used for get cpu elasticsearch history repository config about elasticsearch
+	myCfg esElasticsearchCheckHistoryRepoConfig
+
+	// esCli is elasticsearch client connection injected from the outside package
+	esCli *elasticsearch.Client
+
+	// reqBodyWriter is implementation of reqBodyWriter interface to write []byte for request body
+	reqBodyWriter reqBodyWriter
+}
