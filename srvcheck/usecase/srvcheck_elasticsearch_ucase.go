022f1e76992fa47702278fdd0d35752b04c0fa58
지농 <jinhong0719@naver.com>
2021-03-31 15:37:16 +0900

    [CREATE] <srvcheck/usecase/srvcheck_elasticsearch_ucsae.go> Create file & Add comment

---
+++ srvcheck/usecase/srvcheck_elasticsearch_ucase.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// syscheck_elasticsearch_ucase.go is file that define usecase implementation about elasticsearch check in srvcheck domain
+// elasticsearch check usecase struct embed serviceCheckUsecaseComponent struct in ./srvcheck.go file
+
+package usecase
