eee48bfddb1c7f31726876a4c2afe5f9a99d24a5
지농 <jinhong0719@naver.com>
2021-03-31 15:16:07 +0900

    [ADD] <../elasticsearch/srvcheck_elasticsearch_repo.go> Add Migrate method

--- srvcheck/repository/elasticsearch/srvcheck_elasticsearch_repo.go
+++ srvcheck/repository/elasticsearch/srvcheck_elasticsearch_repo.go
@@ -5,10 +5,17 @@
 package elasticsearch
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/DMS-SMS/v1-health-check/domain"
 	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
 	"github.com/pkg/errors"
 	"log"
+	"net/http"
+	"time"
 )
 
 // esElasticsearchCheckHistoryRepository is to handle ElasticsearchCheckHistoryRepository model using elasticsearch as data store
@@ -46,4 +53,49 @@ func NewESElasticsearchCheckHistoryRepository(
 	}
 
 	return repo
+}
+
+// Implement Migrate method of ElasticsearchCheckHistoryRepository interface
+func (eer *esElasticsearchCheckHistoryRepository) Migrate() error {
+	resp, err := (esapi.IndicesExistsRequest{
+		Index: []string{eer.myCfg.IndexName()},
+	}).Do(context.Background(), eer.esCli)
+
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("failed to call IndicesExists, resp: %+v", resp))
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+		if err := eer.createIndex(); err != nil {
+			return errors.Wrap(err, "failed to create index")
+		}
+	}
+
+	return nil
+}
+
+// createIndex method create index with name, share number in esElasticsearchCheckHistoryRepository
+func (eer *esElasticsearchCheckHistoryRepository) createIndex() error {
+	body := map[string]interface{}{}
+	body["settings.number_of_shards"] = eer.myCfg.IndexShardNum()
+	body["settings.number_of_replicas"] = eer.myCfg.IndexReplicaNum()
+
+	b, _ := json.Marshal(body)
+	if _, err := eer.reqBodyWriter.Write(b); err != nil {
+		return errors.Wrap(err, "failed to write map to body writer")
+	}
+
+	buf := &bytes.Buffer{}
+	if _, err := eer.reqBodyWriter.WriteTo(buf); err != nil {
+		return errors.Wrap(err, "failed to body writer WriteTo method")
+	}
+
+	resp, err := (esapi.IndicesCreateRequest{
+		Index:         eer.myCfg.IndexName(),
+		Body:          bytes.NewReader(buf.Bytes()),
+		MasterTimeout: time.Second * 5,
+		Timeout:       time.Second * 5,
+	}).Do(context.Background(), eer.esCli)
+
+	return errors.Wrap(err, fmt.Sprintf("failed to call IndicesCreate, resp: %+v", resp))
 }
