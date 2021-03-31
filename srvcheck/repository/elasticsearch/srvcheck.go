2cefd06fffd38287fc11d5e852c84dcaf9f64ff3
지농 <jinhong0719@naver.com>
2021-03-31 10:18:42 +0900

    [ADD] <../elasticsearch/srvcheck.go> Add esRepositoryComponentConfig interface

--- srvcheck/repository/elasticsearch/srvcheck.go
+++ srvcheck/repository/elasticsearch/srvcheck.go
@@ -6,3 +6,16 @@
 // It also defines interface or function used jointly in the package as private.
 
 package elasticsearch
+
+// esRepositoryComponentConfig is interface contains method to return config value that elasticsearch repository should have
+// It can be externally set as Config object that implements that interface.
+type esRepositoryComponentConfig interface {
+	// IndexName method returns the index name of elasticsearch about srvcheck
+	IndexName() string
+
+	// IndexShardNum method returns the number of index shard in elasticsearch about srvcheck
+	IndexShardNum() int
+
+	// IndexReplicaNum method returns the number of index replica in elasticsearch about srvcheck
+	IndexReplicaNum() int
+}
