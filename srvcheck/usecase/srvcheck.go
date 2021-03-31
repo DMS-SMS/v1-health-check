04333423a609af466c764c4c2db86b1a1df4e07b
지농 <jinhong0719@naver.com>
2021-03-31 10:04:59 +0900

    [CREATE] <srvcheck/usecase/srvcheck.go> Create file & Add file & pkg description

---
+++ srvcheck/usecase/srvcheck.go
@@ -0,0 +1,9 @@
+// Create package in v.1.0.0
+// usecase package declare implementation of usecase interface about service check(srvcheck) domain
+// all usecase implementation will accept any input from Delivery layer
+// This usecase layer will depends to Repository layer
+
+// srvcheck.go is file that define structure to embed from another structures.
+// It also defines variables or constants, functions used jointly in the package as private.
+
+package usecase
