d1de2884c142a723633fca62c32e421fa132aa56
지농 <jinhong0719@naver.com>
2021-03-31 15:55:02 +0900

    [ADD] <srvcheck/usecase/srvcheck_elasticsearch_ucase.go> Add elasticsearchCheckUsecase

--- srvcheck/usecase/srvcheck_elasticsearch_ucase.go
+++ srvcheck/usecase/srvcheck_elasticsearch_ucase.go
@@ -4,6 +4,11 @@
 
 package usecase
 
+import (
+	"github.com/DMS-SMS/v1-health-check/domain"
+	"sync"
+)
+
 // elasticsearchCheckStatus is type to int constant represent current elasticsearch check process status
 type elasticsearchCheckStatus int
 const (
@@ -12,3 +17,21 @@ const (
 	elasticsearchStatusRecovering                                 // represent it's recovering elasticsearch status now
 	elasticsearchStatusUnhealthy                                  // represent elasticsearch check status is unhealthy
 )
+
+// elasticsearchCheckUsecase implement ElasticsearchCheckUsecase interface in domain and used in delivery layer
+type elasticsearchCheckUsecase struct {
+	// myCfg is used for getting elasticsearch check usecase config
+	myCfg elasticsearchCheckUsecaseConfig
+
+	// historyRepo is used for store elasticsearch check history and injected from outside
+	historyRepo domain.ElasticsearchCheckHistoryRepository
+
+	// slackChat is used for agent slack API about chatting
+	slackChatAgency slackChatAgency
+
+	// status represent current process status of elasticsearch health check
+	status elasticsearchCheckStatus
+
+	// mutex help to prevent race condition when set status field value
+	mutex sync.Mutex
+}
