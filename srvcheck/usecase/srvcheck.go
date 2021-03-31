6bec1a5fd4f2312605ff69c8940f0dbefc5e7698
지농 <jinhong0719@naver.com>
2021-03-31 10:07:33 +0900

    [ADD] <srvcheck/usecsae/srvcheck.go> Add global const variable about process level

--- srvcheck/usecase/srvcheck.go
+++ srvcheck/usecase/srvcheck.go
@@ -7,3 +7,14 @@
 // It also defines variables or constants, functions used jointly in the package as private.
 
 package usecase
+
+// global variable used in usecase to represent process level
+const (
+	healthyLevel      = "HEALTHY"       // represent that service status is healthy now
+	warningLevel      = "WARNING"       // represent that service status is warning now
+	weakDetectedLevel = "WEAK_DETECTED" // represent that weak of service status is detected
+	recoveringLevel   = "RECOVERING"    // represent that recovering weak of service status now
+	recoveredLevel    = "RECOVERED"     // represent that succeed to recover service status
+	unhealthyLevel    = "UNHEALTHY"     // represent that service status is unhealthy now (not recovered)
+	errorLevel        = "ERROR"         // represent that error occurs while checking service status
+)
