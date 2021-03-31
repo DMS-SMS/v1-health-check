cb52767eee8a2899e24ed6e2ec34e496a7f1bb1c
지농 <jinhong0719@naver.com>
2021-03-31 15:48:24 +0900

    [ADD] <srvcheck/usecase/srvcheck_elasticsearch_ucase.go> Add elasticsearchCheckStatus

--- srvcheck/usecase/srvcheck_elasticsearch_ucase.go
+++ srvcheck/usecase/srvcheck_elasticsearch_ucase.go
@@ -1,5 +1,14 @@
 // Create file in v.1.0.0
-// syscheck_elasticsearch_ucase.go is file that define usecase implementation about elasticsearch check in srvcheck domain
+// srvcheck_elasticsearch_ucase.go is file that define usecase implementation about elasticsearch check in srvcheck domain
 // elasticsearch check usecase struct embed serviceCheckUsecaseComponent struct in ./srvcheck.go file
 
 package usecase
+
+// elasticsearchCheckStatus is type to int constant represent current elasticsearch check process status
+type elasticsearchCheckStatus int
+const (
+	elasticsearchStatusHealthy    elasticsearchCheckStatus = iota // represent elasticsearch check status is healthy
+	elasticsearchStatusWarning                                    // represent elasticsearch check status is warning now
+	elasticsearchStatusRecovering                                 // represent it's recovering elasticsearch status now
+	elasticsearchStatusUnhealthy                                  // represent elasticsearch check status is unhealthy
+)
