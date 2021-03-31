936fa78e00870a358655055b75d193722c3c155d
지농 <jinhong0719@naver.com>
2021-03-31 10:11:58 +0900

    [CREATE] <../elasticsearch/srvcheck_elasticsearch_repo.go> Create file & Add comment

---
+++ srvcheck/repository/elasticsearch/srvcheck_elasticsearch_repo.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// srvcheck_elasticsearch_repo.go is file that define implement elasticsearch history repository using elasticsearch
+// this elasticsearch repository struct embed esRepositoryRequiredComponent struct in ./srvcheck.go file
+
+package elasticsearch
