726ce661d62f06f89dfbd32724d427c57bd9492d
지농 <jinhong0719@naver.com>
2021-04-01 15:13:52 +0900

    [CREATE] <elasticsearch/agent.go> Create file & Add file, package description

---
+++ elasticsearch/agent.go
@@ -0,0 +1,8 @@
+// Create package in v.1.0.0
+// elasticsearch package define struct which is implement various interface about elasticsearch agency using in usecase each of domain
+// there are kind of elasticsearch agency function such as get or delete cluster, indices
+
+// in agent.go file, define struct type of elasticsearch agent & initializer that are not method.
+// Also if exist, custom type or variable used in common in each of method will declared in this file.
+
+package elasticsearch
