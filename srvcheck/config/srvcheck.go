b8f1e105a2c0ecad6a2cdfd4040ff55ae2939208
지농 <jinhong0719@naver.com>
2021-04-01 09:05:07 +0900

    [CREATE] <srvcheck/config/srvcheck.go> Create file & Add file, pkg description

---
+++ srvcheck/config/srvcheck.go
@@ -0,0 +1,8 @@
+// Create package in v.1.0.0.
+// config package contains App global variable with config value about srvcheck from environment variable or config file
+// App return field value from method having same name with that field name
+
+// config.go is file that define srvcheckConfig type which is type of App
+// Also, App implement various config interface each of package in srvcheck domain by declaring method
+
+package config
