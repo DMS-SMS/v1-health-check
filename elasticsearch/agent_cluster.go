b94b5656a4c2c2ec567790707e62583c17760c21
지농 <jinhong0719@naver.com>
2021-04-01 16:55:02 +0900

    [ADD] <elasticsearch/agent_cluster.go> Add getClusterHealthResult & method

--- elasticsearch/agent_cluster.go
+++ elasticsearch/agent_cluster.go
@@ -3,3 +3,15 @@
 // implement agency interface about elasticsearch cluster defined in each of domain
 
 package elasticsearch
+
+
+// getClusterHealthResult is implementation of GetClusterHealth return type interface
+type getClusterHealthResult struct {
+	activePrimaryShards, activeShards, unassignedShards, activeShardsPercent float64
+}
+
+// define getClusterHealthResult methods that return field of this struct
+func (result getClusterHealthResult) ActivePrimaryShards() int     { return int(result.activePrimaryShards) }
+func (result getClusterHealthResult) ActiveShards() int            { return int(result.activeShards) }
+func (result getClusterHealthResult) UnassignedShards() int        { return int(result.unassignedShards) }
+func (result getClusterHealthResult) ActiveShardsPercent() float64 { return result.activeShardsPercent }
