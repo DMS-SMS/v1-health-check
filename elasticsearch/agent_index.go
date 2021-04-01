541cea54db4d803e6438c475c648db06569e3eaa
지농 <jinhong0719@naver.com>
2021-04-01 16:58:31 +0900

    [CREATE] <elasticsearch/agent_index.go> Create file & Add file description

---
+++ elasticsearch/agent_index.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// agent_index.go file define method of elasticsearchAgent about index API
+// implement agency interface about elasticsearch cluster defined in each of domain
+
+package elasticsearch
