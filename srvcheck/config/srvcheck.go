40d586713d2864f8f98f22b89377b2d67c57bb59
지농 <jinhong0719@naver.com>
2021-04-01 09:15:27 +0900

    [ADD] <srvcheck/config/srvcheck.go> Add field about elasticsearch index in config

--- srvcheck/config/srvcheck.go
+++ srvcheck/config/srvcheck.go
@@ -12,7 +12,15 @@ var App *srvcheckConfig
 
 // srvcheckConfig having config value and implement various interface about Config by declaring method
 type srvcheckConfig struct {
+	// fields about index information in elasticsearch (implement esRepositoryComponentConfig)
+	// indexName represent name of elasticsearch index including srvcheck history document
+	indexName *string
 
+	// indexShardNum represent shard number of elasticsearch index storing srvcheck history document
+	indexShardNum *int
+
+	// indexReplicaNum represent replica number of elasticsearch index to replace index when node become unable
+	indexReplicaNum *int
 }
 
 // init function initialize App global variable
