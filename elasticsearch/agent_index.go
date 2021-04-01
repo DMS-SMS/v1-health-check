6756623617c821bfd24daa305521abf502c8d04a
지농 <jinhong0719@naver.com>
2021-04-01 19:09:58 +0900

    [ADD] <elasticsearch/agent_index.go> Add GetIndicesWithPatterns method

--- elasticsearch/agent_index.go
+++ elasticsearch/agent_index.go
@@ -3,3 +3,50 @@
 // implement agency interface about elasticsearch cluster defined in each of domain
 
 package elasticsearch
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/pkg/errors"
+	"time"
+)
+
+// GetIndicesWithRegexp return indices list with regexp pattern
+func (ea *elasticsearchAgent) GetIndicesWithPatterns(patterns []string) (indices []string, err error) {
+	var (
+		ctx = context.Background()
+	)
+
+	resp, err := (esapi.CatIndicesRequest{
+		Index:         patterns,
+		Format:        "JSON",
+		MasterTimeout: time.Second * 5,
+	}).Do(ctx, ea.esCli)
+
+	if err != nil {
+		err = errors.Wrap(err, fmt.Sprintf("failed to call CatIndicesRequest, resp: %+v", resp))
+		return
+	} else if resp.IsError() {
+		err = errors.Errorf("CatIndicesRequest return error code, resp: %+v", resp)
+		return
+	}
+
+	var ms []map[string]interface{}
+	if err = json.NewDecoder(resp.Body).Decode(&ms); err != nil {
+		err = errors.Wrap(err, "failed to decode resp body to map slice")
+		return
+	}
+
+	for _, m := range ms {
+		if v, ok := m["index"].(string); ok {
+			indices = append(indices, v)
+		} else {
+			err = errors.Wrap(err, "string index is not in resp map")
+			return
+		}
+	}
+
+	return
+}
