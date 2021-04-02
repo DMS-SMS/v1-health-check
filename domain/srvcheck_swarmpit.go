0b8d6a17c8d241086265e912daf063520bf22168
지농 <jinhong0719@naver.com>
2021-04-02 17:14:04 +0900

    [CREATE] <domain/srvcheck_swarmpit.go> Create file & Add file description

---
+++ domain/srvcheck_swarmpit.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// srvcheck_swarmpit.go is file that declare model struct & repo interface about swarmpit check in srvcheck domain.
+// also, additional method of model struct is declared in this file, too.
+
+package domain
