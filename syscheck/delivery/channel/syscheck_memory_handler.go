0d042c4ffafd42496dfa93293c48c6a3e921be78
지농 <jinhong0719@naver.com>
2021-04-02 17:10:43 +0900

    [MODIFY] <syscheck/delivery/channel/syscheck_memory_handler.go> Change CheckMemory

--- syscheck/delivery/channel/syscheck_memory_handler.go
+++ syscheck/delivery/channel/syscheck_memory_handler.go
@@ -26,22 +26,22 @@ func NewMemoryCheckHandler(c <-chan time.Time, mu domain.MemoryCheckUseCase) {
 	go handler.startListening(c)
 }
 
-// CheckMemory method set context & call usecase CheckMemory method, handle error
-func (mh *memoryCheckHandler) CheckMemory(t time.Time) {
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, "time", t)
-
-	if err := mh.MUsecase.CheckMemory(ctx); err != nil {
-		log.Printf("error occurs in CheckMemory, err: %v", err)
-	}
-}
-
 // startListening method start listening msg from golang channel & stream msg to another method
 func (mh *memoryCheckHandler) startListening(c <-chan time.Time) {
 	for {
 		select {
 		case t := <-c:
-			mh.CheckMemory(t)
+			mh.checkMemory(t)
 		}
+	}
+}
+
+// checkMemory method set context & call usecase CheckMemory method, handle error
+func (mh *memoryCheckHandler) checkMemory(t time.Time) {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "time", t)
+
+	if err := mh.MUsecase.CheckMemory(ctx); err != nil {
+		log.Printf("error occurs in CheckMemory, err: %v", err)
 	}
 }
