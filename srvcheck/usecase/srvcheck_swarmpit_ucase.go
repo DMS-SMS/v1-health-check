f87c6574048bb396c0c86ce32ce37ae50d0f157a
지농 <jinhong0719@naver.com>
2021-04-03 15:44:44 +0900

    [ADD] <srvcheck/usecase/srvcheck_swrampit_ucase.go> Add swarmpitCheckStatus

--- srvcheck/usecase/srvcheck_swarmpit_ucase.go
+++ srvcheck/usecase/srvcheck_swarmpit_ucase.go
@@ -3,3 +3,11 @@
 // usecase layer depend on repository layer and is depended to delivery layer
 
 package usecase
+
+// swarmpitCheckStatus is type to int constant represent current swarmpit check process status
+type swarmpitCheckStatus int
+const (
+	swarmpitStatusHealthy    swarmpitCheckStatus = iota // represent swarmpit check status is healthy
+	swarmpitStatusRecovering                            // represent it's recovering swarmpit status now
+	swarmpitStatusUnhealthy                             // represent swarmpit check status is unhealthy
+)
