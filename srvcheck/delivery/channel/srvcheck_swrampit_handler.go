c0bafa836e3a8caec45d4eded5f1fb64f44e125f
지농 <jinhong0719@naver.com>
2021-04-03 22:55:45 +0900

    [ADD] <../channel/srvcheck_swrampit_handler.go> Add swarmpitCheckHandler

--- srvcheck/delivery/channel/srvcheck_swrampit_handler.go
+++ srvcheck/delivery/channel/srvcheck_swrampit_handler.go
@@ -2,3 +2,9 @@
 // publishing msg to golang channel which is received from outside is not occurred in this package
 
 package channel
+
+// swarmpitCheckHandler is delivered data handler about swarmpit check using usecase layer
+type swarmpitCheckHandler struct {
+	// SUsecase is usecase layer interface which is injected from package outside (maybe, in main)
+	SUsecase domain.SwarmpitCheckUseCase
+}
