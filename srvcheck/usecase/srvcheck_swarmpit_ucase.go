e3606bcdefdd16db731bf3050c6da3dbb47ac41c
지농 <jinhong0719@naver.com>
2021-04-03 15:43:02 +0900

    [CREATE] <srvcheck/usecase/srvcheck_swarmpit_ucase.go> Create file & Add comment

---
+++ srvcheck/usecase/srvcheck_swarmpit_ucase.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// srvcheck_swarmpit_ucase.go is file that define usecase implementation about swarmpit check in srvcheck domain
+// usecase layer depend on repository layer and is depended to delivery layer
+
+package usecase
