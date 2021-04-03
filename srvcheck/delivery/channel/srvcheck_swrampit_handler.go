03b082f70ef07cae7ad3f353fe4d5a95ca77330e
지농 <jinhong0719@naver.com>
2021-04-03 22:52:25 +0900

    [CREATE] <srvcheck/delivery/channel/srvcheck_swarmpit_handler.go> Create file

---
+++ srvcheck/delivery/channel/srvcheck_swrampit_handler.go
@@ -0,0 +1,4 @@
+// in srvcheck_swarmpit_handler.go file, define delivery from channel msg to swarmpit check usecase handler
+// publishing msg to golang channel which is received from outside is not occurred in this package
+
+package channel
