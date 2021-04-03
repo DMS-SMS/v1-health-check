0ec4b0788a9674daa50b43d51788b5a522ebc79d
지농 <jinhong0719@naver.com>
2021-04-03 15:31:43 +0900

    [ADD] <../srvcheck_swarmpit_repo.go> Add esSwarmpitCheckHistoryRepository

--- srvcheck/repository/elasticsearch/srvcheck_swarmpit_repo.go
+++ srvcheck/repository/elasticsearch/srvcheck_swarmpit_repo.go
@@ -3,3 +3,17 @@
 // this elasticsearch repository struct embed esRepositoryRequiredComponent struct in ./srvcheck.go file
 
 package elasticsearch
+
+import "github.com/elastic/go-elasticsearch/v7"
+
+// esSwarmpitCheckHistoryRepository is to handle SwarmpitCheckHistoryRepository model using elasticsearch as data store
+type esSwarmpitCheckHistoryRepository struct {
+	// myCfg is used for get swarmpit history repository config about elasticsearch
+	myCfg esSwarmpitCheckHistoryRepoConfig
+
+	// esCli is elasticsearch client connection injected from the outside package
+	esCli *elasticsearch.Client
+
+	// reqBodyWriter is implementation of reqBodyWriter interface to write []byte for request body
+	reqBodyWriter reqBodyWriter
+}
