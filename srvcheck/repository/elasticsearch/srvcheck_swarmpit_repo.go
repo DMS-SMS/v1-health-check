05808009633ece933ca685c468aec2879c095a56
지농 <jinhong0719@naver.com>
2021-04-03 15:28:37 +0900

    [CREATE] <srvcheck/repository/elasticsearch/srvcheck_swarmpit_repo.go> Create file

---
+++ srvcheck/repository/elasticsearch/srvcheck_swarmpit_repo.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// srvcheck_swarmpit_repo.go is file that define implement swarmpit history repository using elasticsearch
+// this elasticsearch repository struct embed esRepositoryRequiredComponent struct in ./srvcheck.go file
+
+package elasticsearch
