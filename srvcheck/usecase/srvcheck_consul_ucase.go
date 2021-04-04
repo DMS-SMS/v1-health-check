26305c994b485cf34e9f1d7807aa3fdc13770192
지농 <jinhong0719@naver.com>
2021-04-04 22:21:53 +0900

    [ADD] <srvcheck/usecase/srvcheck_consul_ucase.go> Add consulCheckUsecase struct

--- srvcheck/usecase/srvcheck_consul_ucase.go
+++ srvcheck/usecase/srvcheck_consul_ucase.go
@@ -4,6 +4,11 @@
 
 package usecase
 
+import (
+	"github.com/DMS-SMS/v1-health-check/domain"
+	"sync"
+)
+
 // consulCheckStatus is type to int constant represent current consul check process status
 type consulCheckStatus int
 const (
@@ -11,3 +16,21 @@ const (
 	consulStatusRecovering                          // represent it's recovering consul status now
 	consulStatusUnhealthy                           // represent consul check status is unhealthy
 )
+
+// consulCheckUsecase implement ConsulCheckUsecase interface in domain and used in delivery layer
+type consulCheckUsecase struct {
+	// myCfg is used for getting consul check usecase config
+	myCfg consulCheckUsecaseConfig
+
+	// historyRepo is used for store consul check history and injected from outside
+	historyRepo domain.ConsulCheckHistoryRepository
+
+	// slackChat is used for agent slack API about chatting
+	slackChatAgency slackChatAgency
+
+	// status represent current process status of consul health check
+	status consulCheckStatus
+
+	// mutex help to prevent race condition when set status field value
+	mutex sync.Mutex
+}
