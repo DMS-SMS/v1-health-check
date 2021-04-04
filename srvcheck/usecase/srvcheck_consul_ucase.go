2e574ce84b5efee621b0ecba433214984f5919d9
지농 <jinhong0719@naver.com>
2021-04-04 22:16:58 +0900

    [CREATE] <srvcheck/usecase/srvcheck_consul_ucase.go> Create file & Add description

---
+++ srvcheck/usecase/srvcheck_consul_ucase.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// srvcheck_consul_ucase.go is file that define usecase implementation about consul check in srvcheck domain
+// usecase layer depend on repository layer and is depended to delivery layer
+
+package usecase
