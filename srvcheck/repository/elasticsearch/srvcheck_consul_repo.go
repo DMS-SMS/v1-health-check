e8181e046de5c0b5bffe6b93cef412b84d455a15
지농 <jinhong0719@naver.com>
2021-04-04 21:08:21 +0900

    [CREATE] <srvcheck/repository/elasticsearch/srvcheck_consul_repo.go> Create file

---
+++ srvcheck/repository/elasticsearch/srvcheck_consul_repo.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// srvcheck_consul_repo.go is file that define implement consul history repository using elasticsearch
+// this elasticsearch repository struct embed esRepositoryRequiredComponent struct in ./srvcheck.go file
+
+package elasticsearch
