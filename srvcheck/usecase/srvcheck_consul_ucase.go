1b5eebfc27b6c94ffe0df74bc16698eb92cf6e16
지농 <jinhong0719@naver.com>
2021-04-04 22:18:26 +0900

    [ADD] <srvcheck/usecase/srvcheck_consul_ucase.go> Add consulCheckStatus type

--- srvcheck/usecase/srvcheck_consul_ucase.go
+++ srvcheck/usecase/srvcheck_consul_ucase.go
@@ -3,3 +3,11 @@
 // usecase layer depend on repository layer and is depended to delivery layer
 
 package usecase
+
+// consulCheckStatus is type to int constant represent current consul check process status
+type consulCheckStatus int
+const (
+	consulStatusHealthy    consulCheckStatus = iota // represent consul check status is healthy
+	consulStatusRecovering                          // represent it's recovering consul status now
+	consulStatusUnhealthy                           // represent consul check status is unhealthy
+)
