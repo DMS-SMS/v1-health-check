373fbf4bd59029292322fb5af20f4c27b6ec30f6
지농 <jinhong0719@naver.com>
2021-04-04 21:51:18 +0900

    [ADD] <../elasticsearch/syscheck.go> Add esRepositoryMigrator struct & Migrate method

--- syscheck/repository/elasticsearch/syscheck.go
+++ syscheck/repository/elasticsearch/syscheck.go
@@ -7,7 +7,18 @@
 
 package elasticsearch
 
-import "io"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/pkg/errors"
+	"io"
+	"net/http"
+	"time"
+)
 
 // esRepositoryComponentConfig is interface contains method to return config value that elasticsearch repository should have
 // It can be externally set as Config object that implements that interface.
@@ -26,4 +37,45 @@ type esRepositoryComponentConfig interface {
 type reqBodyWriter interface {
 	io.Writer
 	io.WriterTo
+}
+
+// esRepositoryMigrator is struct that Migrate es repository using parameter variable
+type esRepositoryMigrator struct {}
+
+// Migrate method, if index doesn't exist, create index with name and shard number in esRepositoryComponentConfig
+func (erm esRepositoryMigrator) Migrate(cfg esRepositoryComponentConfig, cli *elasticsearch.Client, w reqBodyWriter) error {
+	resp, err := (esapi.IndicesExistsRequest{
+		Index: []string{cfg.IndexName()},
+	}).Do(context.Background(), cli)
+
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("failed to call IndicesExists, resp: %+v", resp))
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+		body := map[string]interface{}{}
+		body["settings.number_of_shards"] = cfg.IndexShardNum()
+		body["settings.number_of_replicas"] = cfg.IndexReplicaNum()
+
+		b, _ := json.Marshal(body)
+		if _, err := w.Write(b); err != nil {
+			return errors.Wrap(err, "failed to write map to body writer")
+		}
+
+		buf := &bytes.Buffer{}
+		if _, err := w.WriteTo(buf); err != nil {
+			return errors.Wrap(err, "failed to body writer WriteTo method")
+		}
+
+		if resp, err := (esapi.IndicesCreateRequest{
+			Index:         cfg.IndexName(),
+			Body:          bytes.NewReader(buf.Bytes()),
+			MasterTimeout: time.Second * 5,
+			Timeout:       time.Second * 5,
+		}).Do(context.Background(), cli); err != nil {
+			return errors.Wrap(err, fmt.Sprintf("failed to call IndicesCreate, resp: %+v", resp))
+		}
+	}
+
+	return nil
 }
