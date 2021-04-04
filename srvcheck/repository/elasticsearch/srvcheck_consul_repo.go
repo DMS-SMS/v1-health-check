d87e3f90542a2937508a8c994fed8e852ec17570
지농 <jinhong0719@naver.com>
2021-04-04 21:15:48 +0900

    [ADD] <../elasticsearch/srvcheck_consul_repo.go> Add Store method of repository

--- srvcheck/repository/elasticsearch/srvcheck_consul_repo.go
+++ srvcheck/repository/elasticsearch/srvcheck_consul_repo.go
@@ -100,3 +100,37 @@ func (ecr *esConsulCheckHistoryRepository) createIndex() error {
 
 	return errors.Wrap(err, fmt.Sprintf("failed to call IndicesCreate, resp: %+v", resp))
 }
+
+// Implement Store method of ConsulCheckHistoryRepository interface
+func (ecr *esConsulCheckHistoryRepository) Store(history *domain.ConsulCheckHistory) (b []byte, err error) {
+	body, _ := json.Marshal(history.DottedMapWithPrefix(""))
+	if _, err = ecr.reqBodyWriter.Write(body); err != nil {
+		err = errors.Wrap(err, "failed to write map to body writer")
+		return
+	}
+
+	buf := &bytes.Buffer{}
+	if _, err = ecr.reqBodyWriter.WriteTo(buf); err != nil {
+		err = errors.Wrap(err, "failed to body writer WriteTo method")
+		return
+	}
+
+	resp, err := (esapi.IndexRequest{
+		Index:        ecr.myCfg.IndexName(),
+		Body:         bytes.NewReader(buf.Bytes()),
+		Timeout:      time.Second * 5,
+	}).Do(context.Background(), ecr.esCli)
+
+	if err != nil {
+		err = errors.Wrap(err, fmt.Sprintf("failed to call IndexRequest, resp: %+v", resp))
+		return
+	} else if resp.IsError() {
+		err = errors.Errorf("IndexRequest return error code, resp: %+v", resp)
+		return
+	}
+
+	result := map[string]interface{}{}
+	_ = json.NewDecoder(resp.Body).Decode(&result)
+	b, _ = json.Marshal(result)
+	return
+}
