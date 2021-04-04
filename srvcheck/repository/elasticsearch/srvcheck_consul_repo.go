f5e2508a5496d7c2243706a500e040950fc4276d
지농 <jinhong0719@naver.com>
2021-04-04 21:09:20 +0900

    [ADD] <../elasticsearch/srvcheck_consul_repo.go> Add esConsulCheckHistoryRepository

--- srvcheck/repository/elasticsearch/srvcheck_consul_repo.go
+++ srvcheck/repository/elasticsearch/srvcheck_consul_repo.go
@@ -3,3 +3,17 @@
 // this elasticsearch repository struct embed esRepositoryRequiredComponent struct in ./srvcheck.go file
 
 package elasticsearch
+
+import "github.com/elastic/go-elasticsearch/v7"
+
+// esConsulCheckHistoryRepository is to handle ConsulCheckHistoryRepository model using elasticsearch as data store
+type esConsulCheckHistoryRepository struct {
+	// myCfg is used for get consul history repository config about elasticsearch
+	myCfg esConsulCheckHistoryRepoConfig
+
+	// esCli is elasticsearch client connection injected from the outside package
+	esCli *elasticsearch.Client
+
+	// reqBodyWriter is implementation of reqBodyWriter interface to write []byte for request body
+	reqBodyWriter reqBodyWriter
+}
