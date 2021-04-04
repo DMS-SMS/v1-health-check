69aa96bf1c4af2bce79685c018b27d8836f67bd2
지농 <jinhong0719@naver.com>
2021-04-04 20:33:44 +0900

    [ADD] <domain/srvcheck_consul.go> Add ConsulCheckHistory struct

--- domain/srvcheck_consul.go
+++ domain/srvcheck_consul.go
@@ -3,3 +3,18 @@
 // also, additional method of model struct is declared in this file, too.
 
 package domain
+
+// ConsulCheckHistory model is used for record consul check history and result
+type ConsulCheckHistory struct {
+	// get required component by embedding serviceCheckHistoryComponent
+	serviceCheckHistoryComponent
+
+	// InstancesNumPerSrv specifies instances number per service in consul
+	InstancesNumPerSrv map[string]int
+
+	// DeregisteredServiceIDs specifies id list of deregistered service in consul check
+	DeregisteredServiceIDs []string
+
+	// IfServiceDeregister specifies if any service in consul was deregistered
+	IfServiceDeregistered bool
+}
