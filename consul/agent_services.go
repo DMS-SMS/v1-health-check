83d1a31733394763c112dc5c2504d0ca6e87dce7
지농 <jinhong0719@naver.com>
2021-04-05 11:07:32 +0900

    [ADD] <consul/agent_services.go> Add GetAllServices method

--- consul/agent_services.go
+++ consul/agent_services.go
@@ -4,6 +4,29 @@
 
 package consul
 
+import (
+	"github.com/pkg/errors"
+)
+
+// GetAllServices method get all services in consul & return services interface implement
+func (ca *consulAgent) GetAllServices() (interface {
+	IDsOf(srv string) (ids []string) // idsOf return id list of instances which are of received srv
+}, error) {
+	srvs, err := ca.cslCli.Agent().Services()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get all services in consul")
+	}
+
+	srvM := services{}
+	for _, srv := range srvs {
+		if _, ok := srvM[srv.Service]; !ok {
+			srvM[srv.Service] = []string{}
+		}
+		srvM[srv.Service] = append(srvM[srv.Service], srv.ID)
+	}
+	return srvM, nil
+}
+
 // services is map binding type having id list per services, and implement GetAllServices return type interface
 type services map[string][]string
 
