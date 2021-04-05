a43c17814fc4d9a85fff6758742fb208a4019e0d
지농 <jinhong0719@naver.com>
2021-04-05 09:16:20 +0900

    [CREATE] <consul/agent_services.go> Create file & Add file description

---
+++ consul/agent_services.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// agent_services.go is file that define method of consulAgent that agent command about services
+// For example in consul command, there are get services, deregister service, etc ...
+
+package consul
