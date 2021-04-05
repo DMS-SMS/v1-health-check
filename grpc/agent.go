5217f254eafbd80a42eddfd22df5ebf33574f0f4
지농 <jinhong0719@naver.com>
2021-04-05 23:17:12 +0900

    [CREATE] <grpc/agent.go> Create file & Add file, package description

---
+++ grpc/agent.go
@@ -0,0 +1,8 @@
+// Create package in v.1.0.0
+// consul package define struct which is implement various interface about gRPC agency using in each of domain
+// there are kind of method in gRPC agency such as ping connection check, etc ...
+
+// in agent.go file, define struct type of gRPC agent & initializer that are not method.
+// Also if exist, custom type or variable used in common in each of method will declared in this file.
+
+package grpc
