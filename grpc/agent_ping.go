7c53280a79da688ec72761e5a3d169851643bf92
지농 <jinhong0719@naver.com>
2021-04-05 23:29:07 +0900

    [ADD] <grpc/agent_ping.go> Add PingToCheckConn method in gRPCAgent

--- grpc/agent_ping.go
+++ grpc/agent_ping.go
@@ -3,3 +3,15 @@
 // For example in consul command, there are ping for connection check, etc ...
 
 package grpc
+
+import (
+	"context"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+)
+
+// PingToCheckConn ping for connection check to gRPC node
+func (ga *gRPCAgent) PingToCheckConn(ctx context.Context, target string, opts ...grpc.DialOption) error {
+	_, err := grpc.DialContext(ctx, target, grpc.WithInsecure(), grpc.WithBlock())
+	return errors.Wrapf(err, "failed to dial gRPC with context, target: %s", target)
+}
