897846bf5544d6f2f6fcdb662c6a86b39db803fe
지농 <jinhong0719@naver.com>
2021-04-05 09:06:10 +0900

    [CREATE] <consul/agent.go> Create file & Add file, package description

---
+++ consul/agent.go
@@ -0,0 +1,8 @@
+// Create package in v.1.0.0
+// consul package define struct which is implement various interface about consul agency using in each of domain
+// there are kind of method in consul agency such as get services, deregister service, etc ...
+
+// in agent.go file, define struct type of consul agent & initializer that are not method.
+// Also if exist, custom type or variable used in common in each of method will declared in this file.
+
+package consul
