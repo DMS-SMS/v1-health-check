21ea8d0690326e63876292c3ce4ccfe357acd4e8
지농 <jinhong0719@naver.com>
2021-04-05 23:25:22 +0900

    [CREATE] <grpc/agent_ping.go> Create file & Add file description

---
+++ grpc/agent_ping.go
@@ -0,0 +1,5 @@
+// Create file in v.1.0.0
+// agent_ping.go is file that define method of gRPCAgent that agent command about ping
+// For example in consul command, there are ping for connection check, etc ...
+
+package grpc
