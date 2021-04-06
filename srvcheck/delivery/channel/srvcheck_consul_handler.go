be19994d8ea63d25120e43bdbfb700243b844cb6
지농 <jinhong0719@naver.com>
2021-04-06 16:04:48 +0900

    [ADD] <../channel/srvcheck_consul_handler.go> Add consulCheckHandler struct

--- srvcheck/delivery/channel/srvcheck_consul_handler.go
+++ srvcheck/delivery/channel/srvcheck_consul_handler.go
@@ -6,3 +6,13 @@
 // publishing msg to golang channel which is received from outside is not occurred in this package
 
 package channel
+
+import (
+	"github.com/DMS-SMS/v1-health-check/domain"
+)
+
+// consulCheckHandler is delivered data handler about consul check using usecase layer
+type consulCheckHandler struct {
+	// CUsecase is usecase layer interface which is injected from package outside (maybe, in main)
+	CUsecase domain.ConsulCheckUseCase
+}
