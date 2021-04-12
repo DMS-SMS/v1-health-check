13a26b26129d5d0f647c53741850aa9f8507d97b
지농 <jinhong0719@naver.com>
2021-04-12 11:25:56 +0900

    [CREATE] <syscheck/delivery/channel/syscheck.go> Create file & Add comment

---
+++ syscheck/delivery/channel/syscheck.go
@@ -0,0 +1,8 @@
+// Create package in v.1.0.0
+// delivery package is for delivery layer acted as presenter layer in syscheck domain which decide how the data will presented
+// in delivery type, could be as REST API, gRPC, golang channel, or HTML file, etc ...
+// in channel delivery, deliver data to usecase by receiving from golang channel while listening
+
+// in this file, define global variable or function using in all struct defined in another file
+
+package channel
