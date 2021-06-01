08b798926bfef7a95bea4481417128d2d2c894e6
지농 <jinhong0719@naver.com>
2021-06-01 20:08:02 +0900

    [ADD] <profiler/default.go> Add defaultProfiler struct

--- profiler/default.go
+++ profiler/default.go
@@ -1,3 +1,19 @@
 package profiler
 
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"os"
+)
 
+// defaultProfiler profile memory and heap usage & save to s3 per a day or when msg come from signal channel
+type defaultProfiler struct {
+	awsSession *session.Session
+	signalChan chan<- os.Signal
+	myCfg      defaultProfilerConfig
+}
+
+// defaultProfilerConfig is the config getter interface about default profiler
+type defaultProfilerConfig interface {
+	// ProfilerS3Bucket method returns string represent profiler s3 bucket
+	ProfilerS3Bucket() string
+}
