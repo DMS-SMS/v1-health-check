2808c0d5f9ec36b186c5c42bb202c4f18bb761aa
지농 <jinhong0719@naver.com>
2021-06-01 22:55:03 +0900

    [MODIFY] <profilier/profilier.go> Change profiler struct & initializer

--- profiler/profilier.go
+++ profiler/profilier.go
@@ -1,16 +1,23 @@
 package profiler
 
 import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"log"
 	"os"
-	"os/signal"
-	"syscall"
+	"runtime"
+	"runtime/pprof"
+	"sync"
+	"time"
 )
 
 // defaultProfiler profile memory and heap usage & save to s3 per a day or when msg come from signal channel
 type defaultProfiler struct {
 	awsSession *session.Session
-	signalChan chan<- os.Signal
+	signalChan chan os.Signal
+	waitGroup  *sync.WaitGroup
 	myCfg      defaultProfilerConfig
 }
 
@@ -18,15 +25,19 @@ type defaultProfiler struct {
 type defaultProfilerConfig interface {
 	// ProfilerS3Bucket method returns string represent profiler s3 bucket
 	ProfilerS3Bucket() string
+
+	// Version method returns string represent version
+	Version() string
 }
 
-func New(s *session.Session, cfg defaultProfilerConfig) *defaultProfiler {
+func New(s *session.Session, wg *sync.WaitGroup, cfg defaultProfilerConfig) *defaultProfiler {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 
 	return &defaultProfiler{
 		awsSession: s,
-		signalChan: sig,
+		signalChan: make(chan os.Signal, 1),
+		waitGroup:  wg,
 		myCfg:      cfg,
 	}
 }
