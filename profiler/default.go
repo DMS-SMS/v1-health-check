dc13bcf2b515071fbecb14acab20de85f6d49fd1
지농 <jinhong0719@naver.com>
2021-06-01 20:08:27 +0900

    [ADD] <profiler/default.go> Add profiler initializer

--- profiler/default.go
+++ profiler/default.go
@@ -3,6 +3,8 @@ package profiler
 import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 // defaultProfiler profile memory and heap usage & save to s3 per a day or when msg come from signal channel
@@ -16,4 +18,15 @@ type defaultProfiler struct {
 type defaultProfilerConfig interface {
 	// ProfilerS3Bucket method returns string represent profiler s3 bucket
 	ProfilerS3Bucket() string
+}
+
+func New(s *session.Session, cfg defaultProfilerConfig) *defaultProfiler {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	return &defaultProfiler{
+		awsSession: s,
+		signalChan: sig,
+		myCfg:      cfg,
+	}
 }
